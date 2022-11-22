e660362995bf93610054e7aec03bfcb39cec31ea
HongHaneul <uhahasky@naver.com>
2022-11-22 18:20:17 +0900

    Create Project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+  created() {
+    console.log("Created!");
+  },
+};
+</script>
